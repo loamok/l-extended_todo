1dcc81e14fc663347a5474164ed150738530733f
Huby Franck <franck.huby@loamok.org>
2020-12-26 12:18:42 +0100

    Api auth with jwt

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -3,18 +3,19 @@
 namespace App\Entity;
 
 use App\Repository\UserRepository;
-use Doctrine\Common\Collections\ArrayCollection;
-use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
 use App\Entity\BehavioursTraits\UuidIdentifiable;
 
-// * @ApiResource(
-// *     normalizationContext={"groups"={"user", "user:read"}},
-// *     denormalizationContext={"groups"={"user", "user:write"}}
-// * )
+use ApiPlatform\Core\Annotation\ApiResource;
+use Symfony\Component\Serializer\Annotation\Groups;
+
 /**
  * @ORM\Entity(repositoryClass=UserRepository::class)
+ * @ApiResource(
+ *     normalizationContext={"groups"={"user", "user:read"}},
+ *     denormalizationContext={"groups"={"user", "user:write"}}
+ * )
  */
 class User implements UserInterface {
     
