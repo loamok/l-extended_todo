7e477859cd19d6ba0bbb57b4a622810006c42f6c
Huby Franck <franck.huby@loamok.org>
2021-01-06 07:50:30 +0100

    Paramètrages des animations

--- assets/interfaces_animations/wtt/interfaces_animations.js
+++ assets/interfaces_animations/wtt/interfaces_animations.js
@@ -3,10 +3,97 @@ const classWttBaseXPath = '.wtt';
 const classDisplayBaseXPath = '.display';
 const wttDisplayCbxp = classWttBaseXPath + classDisplayBaseXPath;
 
-const hiddenClass = 'let-hidden';
-const bgClass = 'let-lightgrey';
-const disabledClass = 'disabled';
+const letHiddenClass = 'let-hidden';
+const letBgClass = 'let-lightgrey';
+const btnOutline = 'btn-outline-';
+const btn = 'btn-';
+const Dark = 'dark';
+const btnOutlineDark = btnOutline + Dark;
+const btnDark = btn + Dark;
+const Warning = 'warning';
+const btnOutlineWarning = btnOutline + Warning;
+const btnWarning = btn + Warning;
+const Success = 'success';
+const btnOutlineSuccess = btnOutline + Success;
+const btnSuccess = btn + Success;
+const Info = 'info';
+const btnOutlineInfo = btnOutline + Info;
+const btnInfo = btn + Info;
+const Danger = 'danger';
+const btnOutlineDanger = btnOutline + Danger;
+const btnDanger = btn + Danger;
+const disabled = 'disabled';
 const btnClass = wttDisplayCbxp + '.actionsBtn';
+
+const btnsClassesToggle = {
+    'btn-calculator-action': {
+        all: {
+            all: {
+                elem: [letBgClass],
+                span: [letHiddenClass]
+            }
+        }
+    },
+    'btn-rewind-action': {
+        beetwenModes: [btnOutlineDark, btnOutlineWarning, letBgClass, disabled],
+        0: {
+            all: {
+                always: [btnOutlineDark, disabled],
+                elem: [letBgClass],
+                span: [letHiddenClass]
+            }
+        },
+        1: {
+            all: {
+                always: [btnOutlineWarning],
+                elem: [letBgClass],
+                span: [letHiddenClass]
+            }
+        }
+    },
+    'btn-playpause-action': {
+        beetwenModes: [btnOutlineSuccess, btnSuccess, letBgClass],
+        0: {
+            all: {
+                always: [btnOutlineSuccess],
+                elem: [letBgClass],
+                span: [letHiddenClass]
+            }
+        },
+        1: {
+            all: {
+                always: [btnSuccess],
+                elem: [],
+                span: [letHiddenClass]
+            }
+        }
+    },
+    'btn-stop-action': {
+        all: {
+            all: {
+                elem: [letBgClass],
+                span: [letHiddenClass]
+            }
+        }
+    },
+    'btn-end-action': {
+        beetwenModes: [btnOutlineDark, btnOutlineWarning, letBgClass, disabled],
+        0: {
+            all: {
+                always: [btnOutlineDark, disabled],
+                elem: [letBgClass],
+                span: [letHiddenClass]
+            }
+        },
+        1: {
+            all: {
+                always: [btnOutlineWarning],
+                elem: [letBgClass],
+                span: [letHiddenClass]
+            }
+        }
+    }
+};
 
 var runMode = 0;
 
@@ -20,27 +107,77 @@ function getSel(elem) {
     return selector;
 }
 
-function setHoverAnim(elem) {
-    const selector = getSel(elem);
+function getConfig(elem) {
+    var id = $(elem).attr('id');
+    var modeOrAll = (btnsClassesToggle[id].hasOwnProperty('all'))?btnsClassesToggle[id].all:btnsClassesToggle[id][runMode];
+    var res = {
+        beetwenModes: (btnsClassesToggle[id].hasOwnProperty('beetwenModes'))?btnsClassesToggle[id].beetwenModes:null,
+        mode: modeOrAll
+    };
     
-    if(!$(elem).hasClass(disabledClass)) {
-        $(selector).removeClass(hiddenClass)
-        $(elem).removeClass(bgClass);
+    return res;
+}
+
+function setOverAnim(elem) {
+    const selector = getSel(elem);
+    const config = getConfig(elem);
+    
+    console.debug(config);
+    
+    if(config.beetwenModes !== null) {
+        for(const className of config.beetwenModes) {
+            $(elem).removeClass(className);
+        }
+    }
+    if(config.mode.all.hasOwnProperty('always')) {
+        for(const className of config.mode.all.always) {
+            $(elem).addClass(className);
+        }
+    }
+    
+    if(!$(elem).hasClass(disabled)) {
+        for(const className of config.mode.all.elem) {
+            $(elem).toggleClass(className);
+        }
+        for(const className of config.mode.all.span) {
+            $(selector).toggleClass(className);
+        }
+//        
+//        $(selector).removeClass(hiddenClass)
+//        $(elem).removeClass(bgClass);
     }
 }
 
 function setOutAnim(elem) {
     const selector = getSel(elem);
+    const config = getConfig(elem);
     
-    $(selector).addClass(hiddenClass);
-    $(elem).addClass(bgClass);
+    if(config.beetwenModes !== null) {
+        for(const className of config.beetwenModes) {
+            $(elem).removeClass(className);
+        }
+    }
+    if(config.mode.all.hasOwnProperty('always')) {
+        for(const className of config.mode.all.always) {
+            $(elem).addClass(className);
+        }
+    }
+    
+    for(const className of config.mode.all.elem) {
+            $(elem).toggleClass(className);
+    }
+    for(const className of config.mode.all.span) {
+        $(selector).toggleClass(className);
+    }
+//    $(selector).addClass(hiddenClass);
+//    $(elem).addClass(bgClass);
 }
 
 
 $(document).ready(function () {
     if($(btnClass).length) {
         $(btnClass).mouseenter(function (e) {
-            setHoverAnim(this);
+            setOverAnim(this);
         });
         $(btnClass).mouseleave(function (e) {
             setOutAnim(this);
